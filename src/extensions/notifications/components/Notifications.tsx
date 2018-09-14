7db25ad8f884b106cd81076c9ed715953d183af5
Daniel Huong | E² <daniel.huong@engagesq.com>
2018-09-14 17:17:27 +1000

    Added method to handle Notifications Box Click

--- src/extensions/notifications/components/Notifications.tsx
+++ src/extensions/notifications/components/Notifications.tsx
@@ -14,33 +14,44 @@ export interface INotificationsProperties {
 
 export interface INotificationsState {
     settingsLoaded: boolean;
+    notificationCount: number;
 }
 
 export default class Notifications extends React.Component<INotificationsProperties, INotificationsState> {
     constructor(props: INotificationsProperties) {
         super(props);
+        const randomInteger = Math.floor(Math.random() * 11);
         this.state = {
-            settingsLoaded: false
+            settingsLoaded: false,
+            notificationCount: randomInteger
         };
     }
 
+    private toggleNotificationsBox(): void {
+        console.log('toggleNotificationsBox() clicked');
+    } 
+
+
     private initComponent() {
-        let stateToUpdate = {...this.state};
-        Settings.Get([
-            Constants.settings.apiURL
-        ]).then(settings => {
-            stateToUpdate.settingsLoaded = true;
-            this.setState(stateToUpdate);
-        });
+        // let stateToUpdate = {...this.state};
+        // Settings.Get([
+        //     Constants.settings.apiURL
+        // ]).then(settings => {
+        //     stateToUpdate.settingsLoaded = true;
+        //     this.setState(stateToUpdate);
+        // });
     }
 
     public render(): React.ReactElement<INotificationsProperties> {
+        const { notificationCount } = this.state;
         console.log('Notifications.tsx: render()');
         return (
             <div className={styles.main}>
-                <div className={styles.container}>
-                    <i className="ms-Icon ms-Icon--Plug"></i>
-                    <span className={styles.count}>3</span>
+                <div 
+                    className={styles.container}
+                    onClick= { () => this.toggleNotificationsBox() }>
+                        <i className="ms-Icon ms-Icon--Plug"></i>
+                        <span className={styles.count}>{notificationCount}</span>
                 </div>
             </div>
         );
