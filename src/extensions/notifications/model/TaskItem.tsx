ac45fc877ce861fcb69d67ad2e3b4853d30071bf
Daniel Huong | E² <daniel.huong@engagesq.com>
2018-09-26 11:23:46 +1000

    Added ID to model

--- src/extensions/notifications/model/TaskItem.tsx
+++ src/extensions/notifications/model/TaskItem.tsx
@@ -3,6 +3,7 @@ export default class TaskItem {
         public title: string,
         public description: Object,
         public startDate: Date,
+        public id: string,
         public linkUrl?: string,
         public endDate?: Date,
         public type?: string,
